d43ae3b3cda3f9b50c57671404cd349b485b9358
medfahmy <medfahmy99@gmail.com>
2022-05-19 10:53:34 +0100

    request refactor

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -5,11 +5,8 @@ use std::io::Write;
 fn main() {
     let mut stream = TcpStream::connect("127.0.0.1:7878").unwrap();
 
-    let args = env::args();
-    let args_str: Vec<String> = args.collect();
-    let str = args_str[1..].join(" ");
+    let args= env::args().collect::<Vec<String>>().join(" ");
+    let buf = args.as_bytes();
 
-    let buffer = str.as_bytes();
-
-    stream.write(&buffer).unwrap();
+    stream.write(buf).unwrap();
 }
